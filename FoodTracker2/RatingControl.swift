063bfcb5d2241eb4d8543106adc8ef79afa6aa36
Quinne Farenwald <quinne.farenwald@MacBook-Pro.local>
2020-02-06 13:52:33 -0700

    Basic RatingControl file structure

---
+++ FoodTracker2/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker2
+//
+//  Created by Quinne Farenwald on 2/6/20.
+//  Copyright © 2020 Quinne Farenwald. All rights reserved.
+//
+
+import UIKit
+ 
+class RatingControl: UIStackView {
+    
+//MARK: Initialization
+   override init(frame: CGRect) {
+       super.init(frame: frame)
+   }
+    
+   required init(coder: NSCoder) {
+       super.init(coder: coder)
+   }
+}
