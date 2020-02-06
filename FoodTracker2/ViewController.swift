b5cd8e11fbd13d5b5d9b7fb6c15decc7549a5c36
Quinne Farenwald <quinne.farenwald@MacBook-Pro.local>
2020-02-06 13:26:11 -0700

    Rename method and key to match xcode latest version

--- FoodTracker2/ViewController.swift
+++ FoodTracker2/ViewController.swift
@@ -40,10 +40,11 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         dismiss(animated: true, completion: nil)
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController,
+    didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         // The info dictionary may contain multiple representations of the image. You want to use the original.
-        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
             fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
         }
         
