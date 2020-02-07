6beb7ab7f050c4b5ea32ea200f31350991769839
Quinne Farenwald <quinne.farenwald@MacBook-Pro.local>
2020-02-07 16:01:22 -0700

    Unwind navigation

--- FoodTracker2/MealTableViewController.swift
+++ FoodTracker2/MealTableViewController.swift
@@ -101,7 +101,7 @@ class MealTableViewController: UITableViewController {
     //MARK: Actions
     
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
-        if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
+        if let sourceViewController = sender.source as? ViewController, let meal = sourceViewController.meal {
             
             // Add a new meal.
             let newIndexPath = IndexPath(row: meals.count, section: 0)
