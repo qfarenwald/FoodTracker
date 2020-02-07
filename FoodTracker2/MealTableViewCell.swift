fa0103d595668e35f1104127f1939ace214fd1e9
Quinne Farenwald <quinne.farenwald@MacBook-Pro.local>
2020-02-07 13:03:55 -0700

    Set up basic custom row structure

---
+++ FoodTracker2/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker2
+//
+//  Created by Quinne Farenwald on 2/7/20.
+//  Copyright © 2020 Quinne Farenwald. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
